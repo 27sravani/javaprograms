d938514b8498991268fc6932a7afce0e39184209
27sravani <sravani1997.chakravaram@gmail.com>
2016-10-03 23:23:48 +0530

    Add files via upload

---
+++ Example1.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+public class Example1
+{
+   public static void main(String args[])
+   {
+      int[] numbers;
+      numbers=new int[5];
+      numbers[0]=10;
+      numbers[1]=20;
+      numbers[2]=30;
+      numbers[3]=40;
+      numbers[4]=50;
+      System.out.println("element at index 0---"+numbers[0]);
+      System.out.println("element at index 1---"+numbers[1]);
+      System.out.println("element at index 2---"+numbers[2]);
+      System.out.println("element at index 3---"+numbers[3]);
+      System.out.println("element at index 4---"+numbers[4]);
+   }
+}
